1574f26730335d9ca9391a143ed9e865a448e42c
Kilmar22 <72371533+Kilmar22@users.noreply.github.com>
2021-02-14 23:53:47 -0600

    firt commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   
+    <HelloWorld msg="Capturador de evento de las teclas del teclado"/>
+
+
+    
   </div>
 </template>
 
 <script>
+
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
